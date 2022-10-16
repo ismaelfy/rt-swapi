2a0a9f688e4b8ed7fb5c32b2732768c28e45ad06
Ismael Fernandez <ismael.7087@gmail.com>
2022-10-16 00:18:43 -0500

    actualizacion test && event

--- src/helper/formatPeopleData.js
+++ src/helper/formatPeopleData.js
@@ -1,21 +1,23 @@
-const FormatPeopleData = (data) => ({
-    nombre: data.name,
-    altura: data.height,
-    masa: data.mass,
-    color_de_pelo: data.hair_color,
-    color_de_piel: data.skin_color,
-    color_de_ojos: data.eye_color,
-    año_nacimiento: data.birth_year,
-    genero: data.gender,
-    mundo_natal: data.homeworld,
-    peliculas: data.films,
-    especies: data.species,
-    vehiculos: data.vehicles,
-    naves_estelares: data.edited,
-    creado: data.created,
-    editado: data.edited,
-    enlace: data.url,
+function FormatPeopleData(data) {
+    return {
+        nombre: data.name,
+        altura: data.height,
+        masa: data.mass,
+        color_de_pelo: data.hair_color,
+        color_de_piel: data.skin_color,
+        color_de_ojos: data.eye_color,
+        año_nacimiento: data.birth_year,
+        genero: data.gender,
+        mundo_natal: data.homeworld,
+        peliculas: data.films,
+        especies: data.species,
+        vehiculos: data.vehicles,
+        naves_estelares: data.edited,
+        creado: data.created,
+        editado: data.edited,
+        enlace: data.url,
+    }
 
-})
+}
 
-module.exports = FormatPeopleData;
+module.exports = FormatPeopleData
